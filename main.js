4d5a85045668560b75b9110f32a3f1b99671456c
SePo <sebastianpopa@live.com>
2023-11-08 17:27:25 +0200

    active navbar links

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 /*==================== toggle icon navbar ====================*/
 /*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-  /*==================== sticky navbar ====================*/
-  /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+window.onscroll = () => {
+   sections.forEach(sec => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+
+      if (top >= offset && top < offset + height) {
+         navLinks.forEach(links => {
+            links.classList.remove('active');
+            document
+               .querySelector('header nav a[href*=' + id + ']')
+               .classList.add('active');
+         });
+      }
+   });
+   /*==================== sticky navbar ====================*/
+   // 50:00
+};
+
+/*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
 
 /*==================== scroll reveal ====================*/
 /*==================== typed js ====================*/
